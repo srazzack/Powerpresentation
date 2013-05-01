09c316e5b3ae1071dc1a1d3d87443df09ef00478
Shafi Razzack <razzack_2002@yahoo.com>
2013-05-01 16:54:02 -0700

    structure additions

--- js/main.js
+++ js/main.js
@@ -21,24 +21,17 @@ var app = {
     }],
     slides: []
 };
+
+var slideObj = {
+	selectedTheme: app.selectedTheme,
+	slides: app.slides
+};
 /* TO DOs: 
 
 */  
 $(document).ready(function() {
 
 	$("#themeOption").html($("#themeTemplate").tmpl(app));
-
-	$("#slideForm").on('submit', function(event){
-		console.log(arguments);
-		var slide = form2js('slideForm');
-		app.slides.push(slide);
-
-		$("#slideNav").html($("#slidebarTemplate").tmpl(app));
-		// update the view to show the latest JSON object
-		$("#ObjectRep").html(JSON.stringify(app.slides, null, '\t'));
-		
-		return false;
-	});
 
 	function resetForm(id) {
 		$('#'+id).each(function(){
@@ -52,15 +45,46 @@ $(document).ready(function() {
 	
 	//Theme and template selection: click with selection saved and mouseover event with toggle, post-click animation with client callback function
 	$('.theme').on('click', function(){
-		console.log(arguments);
+		//console.log(arguments);
 		var themeName = $(this).data('theme-name') + 'theme';
+		console.log(themeName);
 		app.selectedTheme = themeName;
 		//console.log(this);
 		console.log(this.value);
 		$('.theme').fadeOut(1500);
 
-		$("#ppt").html($("#slide").tmpl(app));
+		$("#slideFormContainer").html($("#slideFormTemplate").tmpl(app));
+
+		$("#ppt").html($("#slideTemplate").tmpl({}));
+
+		$("#slideNav").html($("#slidebarTemplate").tmpl(app));
+
+		$("#slideForm").on('submit', function(event){
+			//console.log(arguments);
+			var slide = form2js('slideForm');
+			app.slides.push(slide);
+
+			slide.selectedTheme = app.selectedTheme;
+
+			$("#slideNav").html($("#slidebarTemplate").tmpl(app));
+			// update the view to show the latest JSON object
+			$("#ObjectRep").html(JSON.stringify(app.slides, null, '\t'));
+
+			$("#ppt").html($("#slideTemplate").tmpl(slide));
+
+			$(".slidebarprev").on('click', function(){
+				var slideNumber = $(this).data('slide-index');
+				app.selectedSlide = slideNumber;
+				console.log(this.value);
+				console.log(slideNumber);
+			});
+
+			return false;
+		});
+
 	});
+
+
 
 	//Fade out unselected previews
 	$("#themeSelect :button").on('click', function(){
