f919088a82988e4034fa87e8e9c6dd390b266d6e
Shafi Razzack <razzack_2002@yahoo.com>
2013-05-07 23:57:09 -0700

    organized version draft

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 
 /*Build Log:
   Make theme-preview selection display styles (internally) with a timer
-
-  1. form to json
-  2. make slides deletable
-  3. drag and drop slides*/
+  1. make slides deletable
+  2. drag and drop slides*/
 
 var app = {
 	slidePreviews: "",
@@ -26,102 +24,82 @@ var app = {
     slides: []
 };
 
-/* TO DOs: 
-
-*/  
 $(document).ready(function() {
 
-	$("#themeOption").html($("#themeTemplate").tmpl(app));
-
-	function resetForm(id) {
+	var formResetHandler = function(){
 		$('#'+id).each(function(){
-		        this.reset();
+			this.reset();
 		});
-	}
-	//Dynamic previews of theme templates upon a mouseover event
-	$('.theme').on('mouseover', function(){
-		//mouseover previews using jquery templates?
-	});
-	
-	//Theme and template selection: click with selection saved and mouseover event with toggle, post-click animation with client callback function
-	$('.theme').on('click', function(){
+	},
 
-		$("#themeSelect :button").on('click', function(){
-			$('.theme').fadeIn(1000);
-		});
+		themeOptionHandler = function(){
 
-		//console.log(arguments);
-		var themeName = $(this).data('theme-name') + 'theme';
-		console.log(themeName);
-		app.selectedTheme = themeName;
-		//console.log(this);
-		console.log(this.value);
-		$('.theme').fadeOut(1500);
+			var themeName = $(this).data('theme-name') + 'theme';
+			app.selectedTheme = themeName;
 
-		$("#slideFormContainer").html($("#slideFormTemplate").tmpl(app));
-
-		$("#ppt").html($("#slideTemplate").tmpl({}));
-
-		$("#slideNav").html($("#slidebarTemplate").tmpl(app));
-
-		$("#slideForm").on('submit', function(event){
-			//console.log(arguments);
-			var slide = form2js('slideForm');
-			app.slides.push(slide);
-
-			console.log("here");
-			console.log($(this));
-
-			console.log("here");
-
-			var slideView = {
-				title: slide.title,
-				header: slide.header,
-				content: slide.content
-				};
-				
-			console.log(slideView);
-			
-			$("#ppt").html($("#slideTemplate").tmpl(slideView));
-
+			$('.theme').fadeOut(1500);
+			$("#ppt").html($("#slideTemplate").tmpl({}));
+			$("#slideFormContainer").html($("#slideFormTemplate").tmpl(app));
 			$("#slideNav").html($("#slidebarTemplate").tmpl(app));
-			// update the view to show the latest JSON object
-			$("#ObjectRep").html(JSON.stringify(app.slides, null, '\t'));
+		},
 
-			var slideObj = {
-				slide: slide,
-				selectedTheme: app.selectedTheme
-			};
-
-
-			$("#ppt").html($("#slideTemplate").tmpl(slideObj));
-
-		
-
-		$('.slidebarprev').on('click', function(){
+		slidePreviewHandler = function(){
 
 			var slideNumber = $(this).data('slide-index');
 			app.selectedSlide = slideNumber;
-
-			var slide = app.slides[slideNumber];
-
-			var theme = app.selectedTheme;
+			var slide = app.slides[slideNumber],
+				theme = app.selectedTheme;
 
 			slideObj = {
 				title: slide.title,
 				header: slide.header,
 				content: slide.content,
 				selectedTheme: theme
-				};
+			};
 
-				$("#ppt").html($("#slideTemplate").tmpl(slideObj));
+			$("#ppt").html($("#slideTemplate").tmpl(slideObj));
 
-					$("#slideForm").populate(slideObj);
-			});
+			$("#slideForm").populate(slideObj);
+		},
+
+		slideFormHandler = function(){
+
+			var slide = form2js('slideForm');
+			
+			app.slides.push(slide);
+
+			var slideView = {
+				title: slide.title,
+				header: slide.header,
+				content: slide.content
+			};
+				
+			$("#ppt").html($("#slideTemplate").tmpl(slideView));
+
+			$("#slideNav").html($("#slidebarTemplate").tmpl(app));
+
+			// update the view to show the latest JSON object
+			$("#ObjectRep").html(JSON.stringify(app.slides, null, '\t'));
 
 			return false;
-		});
-	});
-});	
+		},
+
+		themeGhostHandler = function(){
+			$('.theme').fadeIn(1000);
+		},
+
+		themePreviewHandler = function(){};
+
+
+	$("#themeOption").html($("#themeTemplate").tmpl(app));
+	//Dynamic previews of theme templates upon a mouseover event
+	$('.theme').on('mouseover', function(){/*mouseover previews using jquery templates?*/});
+	//Theme and template selection: click with selection saved and mouseover event with toggle, post-click animation with client callback function
+	$('.theme').on('click', themeOptionHandler());
+	$("#themeSelect :button").on('click', themeGhostHandler());
+	$("#slideForm").on('submit', slideFormHandler());
+	$('.slidebarprev').on('click', slidePreviewHandler());
+
+});
 
 
