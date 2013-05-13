a03d58b8fb978591a6f331941ed977bfabd9f464
Shafi Razzack <razzack_2002@yahoo.com>
2013-05-12 19:42:03 -0700

    Code organization build 3
    .on function problems and quits

--- js/main.js
+++ js/main.js
@@ -28,32 +28,38 @@ $(document).ready(function() {
 
 	$("#themeOption").html($("#themeTemplate").tmpl(app));
 
-	var themeOptionHandler = function(){
 
-			console.log(arguments);
-			var themeName = $(this).data('theme-name') + 'theme';
-			app.selectedTheme = themeName;
-			console.log($this);
+	var themeOptionHandler = function(themeName){
+		app.selectedTheme = themeName;
+		//$('.theme').fadeOut(1500);
+		console.log('here with');
+		$("#ppt").html($("#slideTemplate").tmpl({}));
+		$("#slideFormContainer").html($("#slideFormTemplate").tmpl(app));
+		$("#slideNav").html($("#slidebarTemplate").tmpl(app));
+	};
 
-			$('.theme').fadeOut(1500);
-			$("#ppt").html($("#slideTemplate").tmpl({}));
-			$("#slideFormContainer").html($("#slideFormTemplate").tmpl(app));
-			$("#slideNav").html($("#slidebarTemplate").tmpl(app));
-		};
+	var themeSelector = function(selection){
 
-	$('.theme').on('click', themeOptionHandler());
-
-	$("#themeSelect :button").on('click', themeGhostHandler());
-	var themeGhostHandler = function(){
-			$('.theme').fadeIn(1000);
-		};
-	$("#formResetButton :button").on('click', formResetHandler());
-	var formResetHandler = function(){
-		$('#'+id).each(function(){
-			this.reset();
-		});
+		console.log(arguments);
+		var themeName = selection.data('theme-name') + 'theme';
+		console.log(themeName); 
+		themeOptionHandler(themeName);
 	};
 	
+	console.log(themeSelector);
+
+	var themeGhostHandler = function(){
+			$('.theme').fadeIn(1000);
+		};	
+
+	/*$("#themeSelect :button").on('click', themeGhostHandler());
+	
+	var formResetHandler = function(){
+			this.reset();
+		};
+	$("#formResetButton :button").on('click', formResetHandler());*/
+	
+
 	var slidePreviewHandler = function(){
 
 			var slideNumber = $(this).data('slide-index');
@@ -84,17 +90,32 @@ $(document).ready(function() {
 				header: slide.header,
 				content: slide.content
 			};
-				
-			$("#ppt").html($("#slideTemplate").tmpl(slideView));
 
-			$("#slideNav").html($("#slidebarTemplate").tmpl(app));
-
-			// update the view to show the latest JSON object
-			$("#ObjectRep").html(JSON.stringify(app.slides, null, '\t'));
-
-			return false;
+			uiUpdater(slideView, slide);
+			//return false;
 		};
 
+	var uiUpdater = function(slideView, slide){
+
+			if(slideView){
+				//Display Slide Preview
+				$("#ppt").html($("#slideTemplate").tmpl(slideView));
+				//Update and Render Slide Navigation Bar 
+				$("#slideNav").html($("#slidebarTemplate").tmpl(app));
+			}
+
+			if(slide){
+				// update the view to show the latest JSON object
+				$("#ObjectRep").html(JSON.stringify(app.slides, null, '\t'));
+			}
+
+	};
+
+	$('.theme').on('click', function(){
+		selection = $(this);
+		themeSelector(selection);
+		return false;
+	});
 	$("#slideForm").on('submit', slideFormHandler());
 	$('.slidebarprev').on('click', slidePreviewHandler());
 });
