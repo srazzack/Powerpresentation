d0e6405629155891496d02ce465cc23b7d5c33c2
srazzack <srazzack@gmail.com>
2013-10-02 21:56:40 -0700

    models.js formating

--- tests/src/models.js
+++ tests/src/models.js
@@ -34,41 +34,41 @@ var Theme = Backbone.Model.extend({});
 
 var App = Backbone.Model.extend({
 
-		defaults: {
-			version: "1.0",
-			presentations: new PresentationCollection(),
-			themes: new ThemesCollection()
-		},
+	defaults: {
+		version: "1.0",
+		presentations: new PresentationCollection(),
+		themes: new ThemesCollection()
+	},
 
-		initialize: function() {
+	initialize: function() {
 
-			this.on("change", function() {
-			});
-		},
+		this.on("change", function() {
+		});
+	},
 
-		loadApplicationData: function () {
-		},
+	loadApplicationData: function () {
+	},
 
-		addPresentation: function (presentation) {
-			this.get('presentations').add(presentation);
-		},
+	addPresentation: function (presentation) {
+		this.get('presentations').add(presentation);
+	},
 
-		addTheme: function (theme){
-			this.get("themes").add(theme);
-		},
+	addTheme: function (theme){
+		this.get("themes").add(theme);
+	},
 
-		importPresentation: function (presentation) {
-			//file upload from 
-		},
+	importPresentation: function (presentation) {
+		//file upload from 
+	},
 
-		savePresentation: function (presentation) {
-			//save serverside? 
-		},
+	savePresentation: function (presentation) {
+		//save serverside? 
+	},
 
-		exportPresentation: function (presentation) {
-			//convert to pdf, ppt 
-		}
-	});
+	exportPresentation: function (presentation) {
+		//convert to pdf, ppt 
+	}
+});
 
 var Presentation = Backbone.Model.extend({
 	defaults: {
