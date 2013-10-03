82bba805cc47dccafcc9f06433d4345fdaa1acb1
srazzack <srazzack@gmail.com>
2013-10-02 21:54:13 -0700

    backbone testing
    Backbone collections and models

--- tests/spec/backboneTestSpec.js
+++ tests/spec/backboneTestSpec.js
@@ -1,71 +1,97 @@
 describe("Backbone Model Test", function () {
     describe("create app", function () {
         it("should create a new app ", function () {
-            //expect(Toolbelt.array.first([1, 2, 3, 4, 5, 6], 3)).toEqual([1, 2, 3]);
-            console.log('models ready');
-    		var myApp = new App.application();
-    
-        });
-    });
+            var myApp = new App();
 
-    describe("Presentation initialization", function () {
-        it("should create a new Presentation ", function () {
-            console.log('models ready');
-            var myPresentation = new App.presentation();
-            console.log(myPresentation);
+            console.log(myApp);
+
+            var slide = new Slide();
+            slide.set('title', 'intro');
+            var slide2 = new Slide();
+            slide2.set('title', 'welcome to this');
+            var slide3 = new Slide();
+            slide3.set('title', 'conclusion');
+
+            myApp.presentations = new PresentationCollection();
+
+            var presentation = new Presentation();
+            presentation.set('title', 'Intro to Islam');
+
+            presentation.set('slides', new SlidesCollection());
+            presentation.get('slides').add(slide);
+            presentation.get('slides').add(slide2);
+            presentation.get('slides').add(slide3, {at:0});
+
+            expect(presentation.get('slides').length).toBe(3);
+            expect(slide3).toBe(presentation.get('slides').at(0));
+            expect(slide2).toBe(presentation.get('slides').at(2));
+
+            presentation.get('slides').moveSlide(slide3, 2);
+
+            expect(presentation.get('slides').length).toBe(3);
+            expect(slide3).toBe(presentation.get('slides').at(2));
+
+            presentation.get('slides').remove(slide3);
+
+            expect(presentation.get('slides').length).toBe(2);
+            expect(presentation.get('slides').get(slide3)).toBeUndefined();
+
         });
 
-        it("should add a new Presentation with a title", function () {
-            var ppApp = new App.application();
-            var presentation = new App.presentation({title:'Backbone tutorials'});
+        it("should test the addPresentation and addSlide function within App Model and Presentation Model respectively ",function () { 
+
+            var testApp = new App();
+            var testPresentation = new Presentation(); 
+            var testSlide = new Slide();
+            testSlide.set('title', 'This is a test slide');
+            testApp.addPresentation(testPresentation);
+            testPresentation.addSlide(testSlide);
+
+            expect(testApp.get('presentations').length).toBe(1);
+            expect(testPresentation.get('slides').length).toBe(1);
+            expect(testSlide.get('title')).toEqual('This is a test slide');
+
+        });
+
+        it("should should move an added presentation to another specified index ",function () { 
+
+            var newtestApp = new App();
+            var testPresentation1 = new Presentation(); 
+            var testPresentation2 = new Presentation();
+            var testPresentation3 = new Presentation();
+
+            testPresentation1.set('title', 'First presentation');
+            testPresentation2.set('title', 'Second presentation');
+            testPresentation3.set('title', 'Third presentation');
+
+            newtestApp.addPresentation(testPresentation1);
+            newtestApp.addPresentation(testPresentation2);
+            newtestApp.addPresentation(testPresentation3);
+            console.log(newtestApp.get('presentations'));
+            newtestApp.get('presentations').movePresentation(testPresentation2, 0);
+
+            expect(testPresentation2).toBe(newtestApp.get('presentations').at(0));
+
+        });
+
+        it("should set a theme for the presentation with a title", function () {
+            var ppApp = new App ();
+            var presentation = new Presentation({title:'Backbone tutorials'});
             ppApp.addPresentation(presentation);
-            console.log(ppApp.presentation);
+            presentation.setTheme("redTheme");
+            expect(presentation.get('selectedTheme')).toEqual("redTheme");
+        });
 
-            //ppApp.presentations.length // 1
-            //ppApp.presentation // new presentation 
-
+        it("should add and then delete the current presentation ", function () {
+            var myApp = new App();
+            expect(myApp).toBeDefined();
+            var myPresentation = new Presentation();
+            myApp.addPresentation(myPresentation);
+            myPresentation.set('title', "The myPresentation")
+            expect(myPresentation.get('title')).toEqual("The myPresentation");
+            expect(myApp.get('presentations').get(myPresentation)).toBeDefined();
+            myApp.get('presentations').deletePresentation(myPresentation);
+            expect(myApp.get('presentations').get(myPresentation)).toBeUndefined();
         });
     });
-
-  	describe("slide initialization", function () {
-        it("should create a new slide and add it to myPresentation ", function () {
-            console.log('models ready');
-            var myPresentation = new App.presentation();
-    		var mySlide = new App.slide();
-    		myPresentation.addSlide(mySlide);
-    		console.log(mySlide);
-        });
-
-        it("should create and set slide title", function () {
-            console.log('models ready');
-    		var mySlide = new App.slide();
-    		mySlide.setTitle("The First Slide");
-    		console.log(mySlide, mySlide.title);
-        });
-
-        it("should delete the current slide ", function () {
-            console.log('models ready');
-            var myPresentation = new App.presentation();
-    		var mySlide = new App.slide();
-    		myPresentation.deleteSlide(mySlide);
-    		console.log(mySlide);
-        });
-
-        it("should move the current slide ", function () {
-            console.log('models ready');
-            var myPresentation = new App.presentation();
-    		var mySlide = new App.slide();
-    		myPresentation.moveSlide(mySlide);
-    		console.log(mySlide);
-        });
-    });
-
-
-    describe("create a collection of presentations", function () {
-    	it("should create a collection of presentations", function () {
-    		var presentationCollection = new App.collection.presentation();
-    	});
-    })
 });
-
-    
--- tests/src/models.js
+++ tests/src/models.js
@@ -1,53 +1,60 @@
+var SlidesCollection = Backbone.Collection.extend({
 
-//Question number 1: How do we bring in the backend component into this App?
-//Question number 2: How does 
+	model:Slide,
 
-(function () {
-	App = {};
+	deleteSlide: function (slide) {
+		this.remove(slide);
+	},
 
-	App.application = Backbone.Model.extend({
+	moveSlide:function(slide, newIndex) {
+		console.log("moving", slide, newIndex);
+		this.remove(slide);
+		this.add(slide, {at:newIndex});
+	}
+});
+
+var PresentationCollection = Backbone.Collection.extend({
+	
+	model:Presentation,
+
+	deletePresentation: function (presentation) {
+	 	this.remove(presentation);
+	},
+
+	movePresentation: function (presentation, newIndex){
+		this.remove(presentation);
+		this.add(presentation, {at: newIndex});
+	}
+
+});
+
+var ThemesCollection = Backbone.Collection.extend({model:Theme});
+
+var Theme = Backbone.Model.extend({});
+
+var App = Backbone.Model.extend({
 
 		defaults: {
 			version: "1.0",
-			selectedPresentation: 0,
-			presentations: [],
-			themes: []
+			presentations: new PresentationCollection(),
+			themes: new ThemesCollection()
 		},
 
 		initialize: function() {
+
 			this.on("change", function() {
-			savePresentationData: function () {
-            localStorage.setItem("app", JSON.stringify(app));
-            console.log("save event occurs");
-        },
 			});
 		},
 
 		loadApplicationData: function () {
-			//var saved = storage.getPresentationData();
-            //if (saved) {
-                //console.log("Data exists. Here's the data : ", saved);
-                
-                if (app.version == saved.version){
-                    app = saved;
-                    initialSave = true;
-                }
-                else {
-                    alert("version mismatch, storage data will be cleared");
-                    localStorage.clear();
-                }
-                
-            }
-            else {
-                console.log("Data does not exist, save to localStorage");
-            }
 		},
 
 		addPresentation: function (presentation) {
-			// perform validation
-			if(presentation.title.length > 10){
-				this.presentations.push(presentation);
-			}
+			this.get('presentations').add(presentation);
+		},
+
+		addTheme: function (theme){
+			this.get("themes").add(theme);
 		},
 
 		importPresentation: function (presentation) {
@@ -60,176 +67,89 @@
 
 		exportPresentation: function (presentation) {
 			//convert to pdf, ppt 
-		},
-
-		version: function () {
-			var saved = storage.getPresentationData();
-			if(this.version == saved.version){
-				console.log("Data exists. Here's the data: ", saved);
-			}
-			else{
-				alert("version mismatch, storage data will be cleared");
-                    localStorage.clear();
-			}
-		},
-
-		deletePresentation: function (presentation) {
-		 	this.destroy({
-		 		success: function() {
-		 			console.log("the selected presentation has been deleted");
-		 			//Array reindexed
-		 		}
-		 	});
 		}
 	});
 
-	App.presentation = Backbone.Model.extend({
-		defaults: {
-		 	presentationTitle:'Default Title',
-		 	selectedSlide: 0,
-		 	slides:[],
-		 	selectedTheme: "whitetheme"
-		},
+var Presentation = Backbone.Model.extend({
+	defaults: {
+	 	title:"",
+	 	slides: new SlidesCollection(),
+	 	selectedTheme: "whitetheme"
+	},
 
-		validate: function(attributes){
-		    if(attributes.presentationTitle === undefined){
-		    	return "Remember to set a title for your slide.";
-		    }
-    	},
+	validate: function (){
+	    if(this.get("title").length < 3){
+	    	throw new Error("set a title for your presentation");
+	    }
+	},
 
-		initialize: function(){
-			this.on("change:title", function(model){ 
-				console.log('title changed to: ' + model.get("title"));
-			});
-			this.on("change:slides", function(model){
-				console.log("A slide has been added, updated or deleted");
-			});
-			this.on("change", function(model) {
-				console.log( "The selected slide is set to: " + model.get("selectedSlide")); 
-				console.log("The selected theme is set to: " + model.get("selectedTheme"));
-			})
-		},
+	initalize: function(){
 
-		setPresentationTitle: function (title) {
-			//this.presentationTitle = title;
-			
-		 	this.set("presentationTitle", title);
-		 	this.addSlide();
-		},
+		this.validate();
 
-		setTheme: function (theme) {
-			//this.selectedTheme = theme;
-			this.set("selectedTheme", theme);
-		},
+		this.on("change:title", function(model){
+			console.log('title changed to: ' + model.get("title"));
+		});
+		this.on("change:slides", function(model){
+			console.log("A slide has been added, updated or deleted");
+		});
+		this.on("change", function(model) {
+			console.log( "The selected slide is set to: " + model.get("selectedSlide")); 
+			console.log("The selected theme is set to: " + model.get("selectedTheme"));
+		})
+	},
 
-		addSlide: function (slide) {
-			this.slides.push(slide);
-		},
+	setPresentationTitle: function (title) {
+	 	this.set("title", title);
+	},
 
-		moveSlide: function (initialIndex, finalIndex) {
-    		this.slides.splice(finalIndex, 0, this.splice(initialIndex, 1)[0]);
-		},
+	setTheme: function (theme) {
+		//this.selectedTheme = theme;
+		this.set("selectedTheme", theme);
+	},
 
-		deleteSlide: function () {
-		 	that = this;
-		 	this.destroy({
-		 		success: function() {
-		 			console.log("slide has been deleted");
-		 			//Array reindexed
-				}
-		 	});
-		},
+	addSlide: function (slide) {
+		this.get('slides').add(slide);
+	},
 
-		play: function(slide) {
-			slide.toggle();
-		}
-	}); 
+	deleteSlide: function (model) {
+		this.remove(model);
+	}
+});
 
-	/*var myPres = new App.presentation();
-	myPres.setPresentationTitle('hello');
+var Slide = Backbone.Model.extend({
+	defaults: {
+		title: "",
+		header: "Slide Header",
+		content: "Paragraph"
+	},
 
-	myPres.version = '1.0';
+	initalize: function(){
 
-	var myPres3 = {
-		yo: function() {
+		this.validate();
 
-		}
-	};
-	myPres3.version = '2.0';
+		this.on("change:title", function(model){
+			console.log('title changed to: ' + model.get("title"));
+		});
+		this.on("change:header", function(model){
+			console.log("A header has been added, updated or deleted");
+		});
+	},
+	validate: function (){
+		if(this.get("title").length < 3) {
+        	throw new Error("slide must have a title");
+        }
+	},
 
-	myPres.yo = function() {
-	};
+	setTitle: function (title) {
+		this.set("title", title);
+	},
 
-	var myPres2 = new App.presentation();
+	setHeader: function (header) {
+		this.set("header", header);
+	},
 
-	myPres2.protoype.yo = function () {
-
-	};*/
-
-	App.slide = Backbone.Model.extend({
-		defaults: {
-			title: "Slide Title",
-			header: "Slide Header",
-			content: "Paragraph"
-		},
-
-		validate: function (attributes){
-			if(attributes.title.length < 3) {
-	        	throw new Error('slide must have a title');
-            }
-		},
-
-		setTitle: function (title) {
-			this.set("title", title);
-		},
-
-		setHeader: function (header) {
-			this.set("header", header);
-		},
-
-		addContent: function (content) {
-			this.set("content", content);
-		}
-
-	});
-
-	App.collection = {};
-
-	App.collection.presentations = Backbone.Collection.extend({
-		model: App.presentation,
-		localStorage: new Backbone.localStorage('presentations');
-	});
-
-	App.collection.presentation = Backbone.Collection.extend({
-		model: App.slide,
-		localStorage: new Backbone.localStorage('slides');
-	});
-
-	App.AppView = Backbone.View.extend({
-
-		el: 'body',
-
-		initialize: function() {
-			this.render();
-		},
-
-		render: function() {
-			
-			var template = _.template($("#appTemplate").html());
-			var output = template({data: presentation});
-			//$("#mainElement").append(output);
-
-			this.$el.empty();
-			this.$el.append(output);
-
-			return this;
-		}
-
-		el: "#slideView",
-
-		events: {
-			"click #addSlideButton": 'displaySlide',
-			"click #addPresentation": 'newPresentation'
-		}
-	});
-}());
+	addContent: function (content) {
+		this.set("content", content);
+	}
+});
