552234252989be55a060bf9cd866b42d14ae9b80
srazzack <srazzack@gmail.com>
2013-10-19 18:09:16 -0700

    backbone functionality update

--- tests/src/app.js
+++ tests/src/app.js
@@ -8,7 +8,7 @@ sv.render();
 var slides = [{title: "this is the first slide", header: "this is the first header", content: "this is finally the content" },
 {title: "this is the second slide", header: "this is the second header", content: "this is second the content" },
 {title: "this is the third slide", header: "this is the third header", content: "this is third the content" },
-{title: "this is the fourth slide", header: "this is the fourth header", content: "this fourth the content" }]
+{title: "this is the fourth slide", header: "this is the fourth header", content: "this fourth the content" }, {title: "this is the fourth slide", header: "this is the fourth header", content: "this fifth content" }]
 
 var sc = new SlidesCollection();
 sc.add(slides);
--- tests/src/views.activepresentation.js
+++ tests/src/views.activepresentation.js
@@ -4,6 +4,10 @@ var ActivePresentationView = Backbone.View.extend({
 
     initialize: function() {
         this.collection.on("change", this.render, this);
+        this.collection.on("add", this.render, this);
+        this.collection.on("remove", this.render, this);
+
+        $(document).bind('keyup', _.bind(this.keypressHandler, this));
     },
 
     render: function() {
@@ -16,8 +20,17 @@ var ActivePresentationView = Backbone.View.extend({
         "click #slidesBarTemp": "selectedSlideRender"
     },
 
-    selectedSlideRender: function(e){
+    keypressHandler: function(e) {
+        console.log(e.keyCode);
+        if(e.keyCode === 46 || e.keyCode === 189) {
+            // delete
 
+            this.collection.remove(this.collection.at(this.collection.models.length-1));
+            console.log(this.collection.models.length);
+        }
+    },
+
+    selectedSlideRender: function(e){
         var target = $(e.currentTarget);
         var id     = target.attr("data-id");
         model      = this.collection.get(id);
@@ -26,7 +39,14 @@ var ActivePresentationView = Backbone.View.extend({
         sv.render();
     },
 
-    savePresentation: function (e) {
+    addSlide: function() {
+        console.log("here");
+        var slide = {title:"please add a title here", header: "please add a header to your slide here", content: "please add some content here"};
+        this.collection.add(slide);
+        console.log(this.collection);
+    },
+
+    savePresentation: function () {
         console.log(this.collection.save());
     }
 });
